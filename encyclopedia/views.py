00c0d955326165912d01f0c45411561673dbb1f7
Aidan Delany <apdelany1@gmail.com>
2025-04-15 23:25:37 -0400

    routed URLs typed into the web browser to display appropriate page, if no information was found, simple error message appears. Will change to better error page later. Also turned the rendered results into links using a tags on the index.html. These changes simply satisfy the first two items in the specification section. Will work on search next

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import markdown2
+
 
 from . import util
 
@@ -8,3 +11,21 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def slash(request):
+    return HttpResponse("Hello CSS")
+
+def page(request, name):
+    data = util.get_entry(name)
+
+    #if data no
+    if data == None:
+        return render(request, "encyclopedia/page.html", {
+            "noPage": "The page you are looking for was not found."
+        })
+    
+    #if data yes
+    converted_markdown = markdown2.markdown(data)
+    return render(request, "encyclopedia/page.html", {
+        "name": name,
+        "content": converted_markdown
+    })
